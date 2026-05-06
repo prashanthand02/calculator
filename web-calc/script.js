03a86867e1bc79c63aa9752935d7d79985579be1
prashanthans02 <prashanthanand873@gmail.com>
2026-05-06 11:48:02 +0530

    Creating functions for each mathematical operator.

---
+++ web-calc/script.js
@@ -0,0 +1,5 @@
+function add(firstNum , secondNum) {return firstNum + secondNum};
+function sub(firstNum , secondNum) {return firstNum - secondNum};
+function mul(firstNum , secondNum) {return firstNum * secondNum};
+function div(firstNum , secondNum) {return firstNum / secondNum};
+
