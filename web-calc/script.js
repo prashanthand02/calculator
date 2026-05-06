74e9eea5c057faee414800da05052b302c48d94c
prashanthans02 <prashanthanand873@gmail.com>
2026-05-07 00:37:29 +0530

    Adding functions to update the variables and display the value in display section

--- web-calc/script.js
+++ web-calc/script.js
@@ -6,9 +6,9 @@ function mul(firstNum , secondNum) {return firstNum * secondNum};
 function div(firstNum , secondNum) {return firstNum / secondNum};
 
 // variables for each part of the operation
-let firstNum;
+let firstNum = 0;
+let secondNum = 0;
 let operand;
-let secondNum;
 
 // function for taking the variables and performing operation
 function operate(num1, operator, num2) {
@@ -21,5 +21,42 @@ function operate(num1, operator, num2) {
     }else if (operator === `/`) {
         return div(num1 , num2);
     }
-}
+};
 
+// function to update the first number and display the numbers when clicked
+const nums = document.querySelectorAll(`.nums`);
+const display = document.getElementById(`display`);
+display.textContent = ``;
+
+function updateFirstNum() {
+    firstNum = Number(display.textContent);
+};
+
+// function to update operator and display the numbers when clicked
+const signs = document.querySelectorAll(`.operators`);
+function updateOperator(){
+    signs.forEach(sign => {
+        sign.addEventListener(`click`, () => {
+            display.innerHTML = ``;
+            display.append(sign.textContent);
+            operand = sign.textContent;
+        })
+    })    
+};
+updateOperator();
+
+// function to update the second number and display the numbers when clicked
+function updatSecondNum() {
+    secondNum = Number(display.textContent.slice(1));
+};
+
+nums.forEach(num => {
+    num.addEventListener(`click`, () => {
+        display.append(num.textContent);
+        if (operand === undefined){
+            updateFirstNum();
+        } else {
+            updatSecondNum();
+        }  
+    }) 
+});
